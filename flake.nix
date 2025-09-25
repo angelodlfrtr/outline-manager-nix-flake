9175481e8a59ed30981b382a724bd1c9eda0b233
Angelo Delefortrie <angelo.delefortrie@gmail.com>
2025-09-25 14:06:21 +0200

    Update to 1.18.0

--- flake.nix
+++ flake.nix
@@ -6,28 +6,30 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    system = "x86_64-linux";
-  in {
-    packages."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
-      pname = "outline-vpn-manager";
-      version = "1.15.2";
-      src = pkgs.fetchurl {
-        # url = "https://github.com/Jigsaw-Code/outline-apps/releases/download/v${version}/Outline-Client.AppImage";
-        url = "https://s3.amazonaws.com/outline-releases/manager/linux/stable/Outline-Manager.AppImage";
-        hash = "sha256-/2/8k/c0sBIkz5UKuyCQVqaS44zhtZmhB8xTwcvZTx4=";
-      };
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
     in
-      pkgs.appimageTools.wrapType2 {
-        inherit pname version src;
-      };
-  };
+    {
+      packages."${system}".default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          pname = "outline-vpn-manager";
+          version = "1.18.0";
+          src = pkgs.fetchurl {
+            url = "https://s3.amazonaws.com/outline-releases/manager/linux/stable/Outline-Manager.AppImage";
+            hash = "sha256-n0HCaXUJ/2SAd15IKJBlQ4exwF/U0UsNiL5W4FeHVYI=";
+          };
+        in
+        pkgs.appimageTools.wrapType2 {
+          inherit pname version src;
+        };
+    };
 }
